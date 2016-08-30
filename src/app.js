8826986bfa322adaaf33714dd69a6a7766949fa9
ASDAlexey <asdalexey@yandex.ru>
2016-08-30 23:53:26 +0300

    add faveicon

--- src/app.js
+++ src/app.js
@@ -3,10 +3,10 @@ import path from 'path';
 import logger from 'morgan';
 import bodyParser from 'body-parser';
 import engines from 'consolidate';
-// import favicon from 'serve-favicon';
+import favicon from 'serve-favicon';
 
-import routes from './routes/index';
-import users from './routes/test_parents';
+import routesIndex from './routes/index';
+import reotesTestParents from './routes/test_parents';
 
 const app = express();
 
@@ -15,15 +15,14 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 app.engine('pug', engines.pug);
 
-// uncomment after placing your favicon in /public
-// app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(favicon(`${__dirname}/../public/favicon.ico`));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
-app.use('/', routes);
-app.use('/test_parents', users);
+app.use('/', routesIndex);
+app.use('/test_parents', reotesTestParents);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
